0ca7ac20bd3c567aa546f0a5768bd498c590f0b5
faiz <faizulhaque@tenpearls.com>
2016-04-07 16:15:25 +0500

    override bug fixed from global.Packages

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 "use strict";
 
-global.Packages = {
-    DateFormat: require("dateformat"),
-    Lodash: require("lodash"),
-    Winston: require("winston"),
-    DailyRotateFile: require('winston-daily-rotate-file')
-};
-
 module.exports = require('./lib');
